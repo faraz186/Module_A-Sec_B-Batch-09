e3e8334041022066686bc2027aefce918883cdcc
Muhammad Faraz <farazmohammad2001@gmail.com>
2023-09-05 20:47:50 +0500

    Adding Files

---
+++ Class 12 (JS) (05-09-2023)/app.js
@@ -0,0 +1,98 @@
+// alert("Welcome to JS. \n Let's start chapter 1");
+
+// document.write("Welcome to JS. <br> Let's start chapter 1")
+
+// console.log("Welcome to JS. Let's start chapter 1");
+
+// Swal.fire(
+//     'Login Successfully!',
+//     'You clicked the button!',
+//     'success'
+//   )
+
+// alert("hello world")
+
+
+// var firstname = "abc";
+
+// var lastname = "xyz";
+
+// var fullname = firstname + lastname
+
+// console.log(fullname)
+
+
+// var num = 10;
+
+// var res = num + 5;
+
+// console.log(res)
+
+// var firstname = 10;
+
+// console.log(typeof firstname)
+
+
+// var cal = (12 + 10) * 4;
+
+// var num1 = 5;
+
+// var num2 = 10
+
+// console.log(num1 % num2)
+
+
+// var num = 10;
+
+// var res = num--;
+
+// console.log(num);
+
+// var a = 4;
+
+// var b = 3;
+
+// var inc = --a + b++ + b++ - ++a - b--;
+//           3  + 3  + 4 - 4 - 5
+// console.log(inc)
+
+// -6,1,-4,3,2,1
+// num = num + 1;
+
+// var res = num + 1;
+
+// console.log(num + 1);
+
+// var firstname = "Muhammad";
+
+// var lastname = "faraz";
+
+// console.log("Welcome to " + firstname + " "+ lastname + " Hope you are fine")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
