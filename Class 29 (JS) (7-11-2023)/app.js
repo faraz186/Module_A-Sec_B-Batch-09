88609874e945ad86051252a0e73cc72387d5bf86
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-11-07 21:07:17 +0500

    Add files via upload

---
+++ Class 29 (JS) (7-11-2023)/app.js
@@ -0,0 +1,135 @@
+var firebaseConfig = {
+  apiKey: "********************************************",
+  authDomain: "****************************",
+  projectId: "************************",
+  storageBucket: "**************************",
+  messagingSenderId: "****************",
+  appId: "****************************************",
+};
+
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+
+// *********************Sign Up Auth**********************
+
+function signUp() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(user);
+      firebase
+        .auth()
+        .currentUser.sendEmailVerification()
+        .then(() => {
+          alert("email verification sent..");
+        });
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+}
+
+// *********************Login Auth**********************
+
+function login() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+}
+
+function forgotPassword() {
+  var email = document.getElementById("email");
+
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email.value)
+    .then(() => {
+      alert("password reset email sent..");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+}
+
+function loginWithGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+
+function loginWithGithub() {
+  var provider = new firebase.auth.GithubAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      var token = credential.accessToken;
+
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      // IdP data available in result.additionalUserInfo.profile.
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
