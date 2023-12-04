e89ed7a1d8cced53f0ca7e0937c29aaa1b82cbcd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-12-04 17:12:27 +0500

    Update app.js

--- Class 20 (JS) (06-10-2023)/app.js
+++ Class 20 (JS) (06-10-2023)/app.js
@@ -1,41 +1,41 @@
-// var day = prompt("Enter what day is today");
+var day = prompt("Enter what day is today");
 
-// switch (day) {
-//   case "mon":
-//     alert("Whoopee!");
-//     break;
-//   case "tues":
-//     alert("Whoopee!");
-//     break;
-//   case "wed":
-//     alert("Whoopee!");
-//     break;
-//   case "thurs":
-//     alert("Whoopee!");
-//     break;
-//   case "fri":
-//     alert("Whoopee!");
-//     break;
-//   case "sat":
-//     alert("Whoopee!");
-//     break;
-//   default:
-//     alert("family day..");
-// }
+switch (day) {
+  case "mon":
+    alert("Whoopee!");
+    break;
+  case "tues":
+    alert("Whoopee!");
+    break;
+  case "wed":
+    alert("Whoopee!");
+    break;
+  case "thurs":
+    alert("Whoopee!");
+    break;
+  case "fri":
+    alert("Whoopee!");
+    break;
+  case "sat":
+    alert("Whoopee!");
+    break;
+  default:
+    alert("family day..");
+}
 
 // *********************For loop****************************
 
-// for (var i = 0; i < 0; i++) {
-//   console.log("for loop", i);
-// }
+for (var i = 0; i < 0; i++) {
+  console.log("for loop", i);
+}
 
 // *********************While loop****************************
 
-// var i = 0;
-// while (i < 0) {
-//   console.log("while loop", i);
-//   i++;
-// }
+var i = 0;
+while (i < 0) {
+  console.log("while loop", i);
+  i++;
+}
 // *********************do-While loop****************************
 
 /*var i = 0;
