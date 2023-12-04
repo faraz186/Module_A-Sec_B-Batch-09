4978827b9597a90564a8d24a74eb1512e62f9bfc
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-12-04 17:15:21 +0500

    Update app.js

--- Class 19 (JS) (03-10-2023)/app.js
+++ Class 19 (JS) (03-10-2023)/app.js
@@ -13,84 +13,84 @@ popUp();
 popUp();
 popUp();
 
-// for (var i = 0; i <= 6; i++) {
-//   popUp();
-// }
+for (var i = 0; i <= 6; i++) {
+  popUp();
+}
 
-// function greet() {
-//   var firstname = prompt("Enter first name");
-//   var lastname = prompt("Enter last name");
-//   var fullname = firstname + lastname;
-//   console.log(fullname);
-// }
+function greet() {
+  var firstname = prompt("Enter first name");
+  var lastname = prompt("Enter last name");
+  var fullname = firstname + lastname;
+  console.log(fullname);
+}
 
-// greet();
+greet();
 
-// function sum(a, b) {
-//   // parameter
-//   //   var a = 20,
-//   //     b = 30;
-//   console.log(a + b);
-// }
+function sum(a, b) {
+  // parameter
+  //   var a = 20,
+  //     b = 30;
+  console.log(a + b);
+}
 
-// sum(10, 50); // arguments
+sum(10, 50); // arguments
 
-// function multiplicationTable(tableNumber, tableLength) {
-//   for (var i = 1; i <= tableLength; i++) {
-//     document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br>");
-//   }
-// }
+function multiplicationTable(tableNumber, tableLength) {
+  for (var i = 1; i <= tableLength; i++) {
+    document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br>");
+  }
+}
 
-// var tableNumber = +prompt("Enter a number to generate table...");
-// var tableLength = +prompt("Enter table length..");
+var tableNumber = +prompt("Enter a number to generate table...");
+var tableLength = +prompt("Enter table length..");
 
-// multiplicationTable(tableNumber, tableLength);
+multiplicationTable(tableNumber, tableLength);
 
-// function sum(num1, num2) {
-//   return num1 + num2;
-// }
+function sum(num1, num2) {
+  return num1 + num2;
+}
 
-// var res = sum(10, 50);
+var res = sum(10, 50);
 
-// console.log(res);
+console.log(res);
 
-// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// var res = arr.slice(0, 5);
+var res = arr.slice(0, 5);
 
-// arr.push("faraz");
+arr.push("faraz");
 
-// console.log(res);
+console.log(res);
 
-// function calc(num1, opr, num2) {
-//   if (opr === "+") {
-//     return num1 + num2;
-//   } else if (opr === "-") {
-//     return num1 - num2;
-//   } else if (opr === "*") {
-//     return num1 * num2;
-//   } else if (opr === "/") {
-//     return num1 / num2;
-//   }
-// }
+function calc(num1, opr, num2) {
+  if (opr === "+") {
+    return num1 + num2;
+  } else if (opr === "-") {
+    return num1 - num2;
+  } else if (opr === "*") {
+    return num1 * num2;
+  } else if (opr === "/") {
+    return num1 / num2;
+  }
+}
 
-// var num1 = prompt("Enter first value..");
-// var operator = prompt("Enter operator value..");
-// var num2 = prompt("Enter Second value..");
+var num1 = prompt("Enter first value..");
+var operator = prompt("Enter operator value..");
+var num2 = prompt("Enter Second value..");
 
-// var result = calc(num1, operator, num2);
+var result = calc(num1, operator, num2);
 
-// console.log(result);
+console.log(result);
 
-// var a = "world";
+var a = "world";
 
-// function greet() {
-//   a = "hello World";
-// }
+function greet() {
+  a = "hello World";
+}
 
-// greet();
+greet();
 
-// console.log(a);
+console.log(a);
 
 // *************************Switch Statement***********************
 
